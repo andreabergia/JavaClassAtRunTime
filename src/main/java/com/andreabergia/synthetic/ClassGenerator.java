a72b46a681a078c512566538cfab18c74848c7c1
Andrea Bergia <andreabergia@gmail.com>
2023-09-29 16:46:36 +0200

    feat: add fields and initialize them in the constructor

--- src/main/java/com/andreabergia/synthetic/ClassGenerator.java
+++ src/main/java/com/andreabergia/synthetic/ClassGenerator.java
@@ -4,52 +4,123 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 
 import java.lang.invoke.MethodHandles;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.objectweb.asm.Opcodes.*;
 
 public class ClassGenerator {
-    private static final AtomicInteger counter = new AtomicInteger();
-
     public Class<?> generate(Map<String, Object> properties) throws IllegalAccessException {
-        byte[] classBytes = generateClass();
+        // TODO: cache
+        byte[] classBytes = new SingleClassGenerator(properties).generate();
         return MethodHandles.lookup().defineClass(classBytes);
     }
 
-    private byte[] generateClass() {
-        ClassWriter cw = startClass();
-        generateConstructor(cw);
-        cw.visitEnd();
-        return cw.toByteArray();
-    }
+    private static final class SingleClassGenerator {
+        private static final AtomicInteger counter = new AtomicInteger();
 
-    private ClassWriter startClass() {
-        ClassWriter cw  = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES)  ;
-        cw.visit(
-                V1_7,
-                ACC_PUBLIC | ACC_SUPER | ACC_SYNTHETIC,
-                "com/andreabergia/synthetic/synthesized" + counter.getAndIncrement(),
-                null,
-                "java/lang/Object",
-                new String[0]
-        );
-        return cw;
-    }
+        private final String generatedClassName;
+        private final ClassWriter cw;
+        private final Map<String, Object> recordProperties;
+        private final Map<String, String> fieldTypes;
 
-    private void generateConstructor(ClassWriter cw) {
-        MethodVisitor mw = cw.visitMethod(
-                ACC_PUBLIC,
-                "<init>",
-                "()V",
-                null,
-                null
-        );
-        mw.visitCode();
-        mw.visitVarInsn(ALOAD, 0);
-        mw.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
-        mw.visitInsn(RETURN);
-        mw.visitMaxs(-1, -1);
-        mw.visitEnd();
+        public SingleClassGenerator(Map<String, Object> recordProperties) {
+            this.generatedClassName = "com/andreabergia/synthetic/synthesized" + counter.getAndIncrement();
+            this.cw = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+            this.recordProperties = recordProperties;
+            this.fieldTypes = new HashMap<>(recordProperties.size());
+        }
+
+        private byte[] generate() {
+            startClass();
+            generateFields();
+            generateConstructor();
+            cw.visitEnd();
+            return cw.toByteArray();
+        }
+
+        private ClassWriter startClass() {
+            cw.visit(
+                    V1_7,
+                    ACC_PUBLIC | ACC_SUPER | ACC_SYNTHETIC,
+                    generatedClassName,
+                    null,
+                    "java/lang/Object",
+                    new String[0]
+            );
+            return cw;
+        }
+
+        private void generateFields() {
+            for (Map.Entry<String, Object> mapEntry : recordProperties.entrySet()) {
+                String fieldName = toFieldName(mapEntry.getKey());
+
+                String fieldType;
+                if (mapEntry.getValue() == null) {
+                    fieldType = "java/lang/Object";
+                } else {
+                    fieldType = getFieldType(mapEntry.getValue().getClass());
+                }
+
+                cw.visitField(ACC_PUBLIC, fieldName, toFieldDescriptor(fieldType), null, null);
+                fieldTypes.put(fieldName, fieldType);
+
+                // TODO: add jackson annotation JsonProperty
+            }
+        }
+
+        private String getFieldType(Class<?> aClass) {
+            return aClass.getName().replace(".", "/");
+        }
+
+        private void generateConstructor() {
+            MethodVisitor mw = cw.visitMethod(
+                    ACC_PUBLIC,
+                    "<init>",
+                    "(Ljava/util/Map;)V",
+                    null,
+                    null
+            );
+            mw.visitCode();
+            mw.visitVarInsn(ALOAD, 0);
+            mw.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+
+            fillFieldsValues(mw);
+
+            mw.visitInsn(RETURN);
+            mw.visitMaxs(-1, -1);
+            mw.visitEnd();
+        }
+
+        private void fillFieldsValues(MethodVisitor mw) {
+            for (String propertyName : recordProperties.keySet()) {
+                String fieldName = toFieldName(propertyName);
+                String fieldType = fieldTypes.get(fieldName);
+
+                // Save the current instance of the class on the stack
+                mw.visitVarInsn(ALOAD, 0);
+
+                // Invoke map::get
+                mw.visitVarInsn(ALOAD, 1);
+                mw.visitLdcInsn(propertyName);
+                mw.visitMethodInsn(INVOKEINTERFACE, "java/util/Map", "get", "(Ljava/lang/Object;)Ljava/lang/Object;", true);
+
+                // Cast result appropriately
+                mw.visitTypeInsn(CHECKCAST, fieldType);
+
+                // Save it in the object
+                mw.visitFieldInsn(PUTFIELD, generatedClassName, fieldName, toFieldDescriptor(fieldType));
+            }
+        }
+
+        private String toFieldName(String propertyName) {
+            // TODO: normalize any invalid character
+            return propertyName;
+        }
+
+        private String toFieldDescriptor(String fieldType) {
+                return "L" + fieldType + ";";
+        }
     }
 }
--- src/test/java/com/andreabergia/synthetic/ClassGeneratorTest.java
+++ src/test/java/com/andreabergia/synthetic/ClassGeneratorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.FAIL_ON_EMPTY_BEANS;
@@ -24,13 +25,34 @@ public class ClassGeneratorTest {
     @Test
     public void canGenerateClassFromEmptyMap() throws InvocationTargetException, InstantiationException, IllegalAccessException, JsonProcessingException {
         Map<String, Object> record = Map.of();
+        var object = createClassInstance(record);
+        var serialized = objectMapper.writeValueAsString(object);
+        assertEquals("{}", serialized);
+    }
 
+    @Test
+    public void canGenerateClassFromMapWithOneField() throws InvocationTargetException, InstantiationException, IllegalAccessException, JsonProcessingException {
+        Map<String, Object> record = Map.of("name", "Andrea");
+        var object = createClassInstance(record);
+        var serialized = objectMapper.writeValueAsString(object);
+        assertEquals("{\"name\":\"Andrea\"}", serialized);
+    }
+
+    @Test
+    public void canGenerateClassFromMapWithTwoFields() throws InvocationTargetException, InstantiationException, IllegalAccessException, JsonProcessingException {
+        Map<String, Object> record = new LinkedHashMap<>();
+        record.put("age", 37);
+        record.put("ageOfDeath", null);
+
+        var object = createClassInstance(record);
+        var serialized = objectMapper.writeValueAsString(object);
+        assertEquals("{\"age\":37,\"ageOfDeath\":null}", serialized);
+    }
+
+    private static Object createClassInstance(Map<String, Object> record) throws IllegalAccessException, InstantiationException, InvocationTargetException {
         var theClass = new ClassGenerator().generate(record);
         var constructors = theClass.getConstructors();
         assertEquals(1, constructors.length);
-
-        var object = constructors[0].newInstance();
-        var serialized = objectMapper.writeValueAsString(object);
-        assertEquals("{}", serialized);
+        return constructors[0].newInstance(record);
     }
 }
